4cf0ce46bf6e06eff4190fa07a479f6dcc2c5bda
Scott Fradkin <scott@fradkin.com>
2015-11-27 12:12:11 -0600

    restructured code a bit to create an id for each synth object, store notes with a synth object, clear out notes on each synth define so they don't duplicate

--- tone-snap-new.js
+++ tone-snap-new.js
@@ -1,22 +1,60 @@
-var toneArray = [];
+var toneMap = {};
 
-var addToToneArray = function(toneObj) {
-  toneArray.push(toneObj);
+var addSynthToToneMap = function(toneSynth) {
+  var existingSynth = toneMap[toneSynth.id];
+
+  if (!existingSynth) {
+    toneMap[toneSynth.id] = toneSynth;
+  } else {
+    // reset notes array
+    existingSynth.clearNotes();
+  }
+};
+
+var addNoteToToneMap = function(id, noteObj) {
+  var existingSynth = toneMap[id];
+
+  if (existingSynth) {
+    existingSynth.addNote(noteObj);
+  }
 };
 
 var createAndPlaySynth = function() {
 
-  var synth = new Tone.MonoSynth({type: 'sine'});
-  synth.toMaster();
+  var keys = Object.keys(toneMap);
+
+  // each key is a synth with notes that we need to play
+  // for now, let's just get a single synth to work, so
+  // just use the first key
+
+  var aSynthObj = toneMap[keys[0]];
+  var synth = aSynthObj.synth;
+  var notes = aSynthObj.notes;
+
   var trigTime = '0.01';
 
-    console.log('going through tone array');
-      toneArray.forEach(function(entry) {
-        console.log('triggering note');
-        synth.triggerAttackRelease(entry.note, entry.time, '+' + trigTime);
-        trigTime = trigTime + ' + ' + entry.time;
-      });
+  console.log('going through notes array for synth obj: ' + aSynthObj.id);
+  notes.forEach(function(entry) {
+    console.log('triggering note');
+    synth.triggerAttackRelease(entry.note, entry.time, '+' + trigTime);
+    trigTime = trigTime + ' + ' + entry.time;
+  });
 
+};
+
+ToneSynth.prototype = new Object();
+function ToneSynth(id, synth) {
+  this.id = id;
+  this.synth = synth;
+  this.notes = [];
+};
+
+ToneSynth.prototype.addNote = function(noteobj) {
+  this.notes.push(noteobj);
+};
+
+ToneSynth.prototype.clearNotes = function() {
+  this.notes = [];
 };
 
 ToneBlockMorph.prototype = new CommandBlockMorph();
@@ -27,7 +65,7 @@ function ToneBlockMorph() {
     this.init();
 };
 
-ToneBlockMorph.prototype.init = function () {
+ToneBlockMorph.prototype.init = function() {
     ToneBlockMorph.uber.init.call(this);
 };
 
@@ -35,7 +73,7 @@ ToneBlockMorph.prototype.isTone = function() {
   return true;
 }
 
-ToneBlockMorph.prototype.receiveUserInteraction = function (interaction) {
+ToneBlockMorph.prototype.receiveUserInteraction = function(interaction) {
     var stage = this.parentThatIsA(StageMorph),
         procs = [],
         hats;
@@ -47,35 +85,36 @@ ToneBlockMorph.prototype.receiveUserInteraction = function (interaction) {
     return procs;
 };
 
-Process.prototype.toneSimpleSynth = function (body) {
+ToneBlockMorph.prototype.reactToTemplateCopy = function() {
+  ToneBlockMorph.uber.reactToTemplateCopy.call(this);
+  this.id = '' + (Math.random() * (100 - 0) + 0);
+};
 
-    var outer = this.context.outerContext;
-    this.popContext();
-        if (body) {
-            this.pushContext(body.blockSequence(), outer);
-        }
-    this.pushContext();
+Process.prototype.toneSimpleSynth = function (body) {
+  console.log('id of simple synth: ' + this.context.expression.id);
+  // create the synth object here and store it in the toneMap
+
+  var synth = new Tone.MonoSynth({type: 'sine'});
+  synth.toMaster();
+
+  var toneSynth = new ToneSynth(this.context.expression.id, synth);
+
+  addSynthToToneMap(toneSynth);
+
+  var outer = this.context.outerContext;
+  outer.expression = this.context.expression.id;
+  this.popContext();
+      if (body) {
+          this.pushContext(body.blockSequence(), outer);
+      }
+  this.pushContext();
 };
 
 Process.prototype.toneTest = function(note, time) {
-  console.log('in Process.toneTest function');
+  var outerId = this.context.outerContext.expression;
+  console.log('in Process.toneTest function, outerId = ' + outerId);
 
-  addToToneArray({note: note, time: time});
+  addNoteToToneMap(outerId, {note: note, time: time});
   return null;
 
 };
-
-Process.prototype.tonePlay = function() {
-
-var synth = new Tone.MonoSynth({type: 'sine'});
-synth.toMaster();
-var trigTime = '0.01';
-
-  console.log('going through tone array');
-    toneArray.forEach(function(entry) {
-      console.log('triggering note');
-      synth.triggerAttackRelease(entry.note, entry.time, '+' + trigTime);
-      trigTime = trigTime + ' + ' + entry.time;
-    });
-
-}
