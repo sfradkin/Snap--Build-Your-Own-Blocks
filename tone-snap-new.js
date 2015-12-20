9aa6b8cf09f0fc38ec25ac1c7378c8636ea6611b
Scott Fradkin <scott@fradkin.com>
2015-12-20 00:10:12 -0600

    modifications to cleanup upon stop, change how scheduling callback works so that each synth obj contains it's own callback function for note scheduling that Tone calls

--- tone-snap-new.js
+++ tone-snap-new.js
@@ -27,8 +27,6 @@ var scheduleNote = function(id, noteObj, store) {
     // update the virtual time of the synth
     existingSynth.curVirtTime = existingSynth.curVirtTime + noteObj.seconds;
 
-    //existingSynth.nextTime = existingSynth.synth.toSeconds('+' + noteObj.time, existingSynth.nextTime);
-
     existingSynth.offSet += existingSynth.synth.toSeconds(noteObj.time);
     existingSynth.offSetTicks = Tone.Transport.toTicks(existingSynth.offSet);
     console.log('offset time in seconds: ', existingSynth.offSet, ' offset time in ticks: ', existingSynth.offSetTicks);
@@ -44,14 +42,14 @@ var createAndPlaySynth = function(synthid) {
 
   console.log('at end of synth block: ', synthid);
 
-  // set a flag on each of the synths to tell it to use "recorded mode"
   // kick off "recorded mode playback"
   var keys = Object.keys(toneMap);
 
   keys.forEach(function(key) {
     synthObj = toneMap[key];
-    synthObj.recordedMode = true;
-    synthObj.totalLoopOffset = synthObj.offSet;
+    if (!synthObj.scheduled) {
+      synthObj.totalLoopOffset = synthObj.offSet;
+    }
   });
 
   doToneRecordedMode();
@@ -73,43 +71,25 @@ var stopTone = function() {
     synthObj = toneMap[key];
     synthObj.synth.dispose();
   });
+
+  toneMap = {};
+  Tone.Transport.cancel();
 };
 
 var doToneRecordedMode = function() {
   var synthObj, notes;
-  var loopStart = {};
-  var loopEnd = {};
+
   var keys = Object.keys(toneMap);
 
     keys.forEach(function(key) {
 
       synthObj = toneMap[key];
-
-      console.log('time now: ', Tone.Transport.now(), ', schedule periodic callback 1 for synth id: ', synthObj.id, ' every ', (synthObj.loopTime - 0.05));
-      Tone.Transport.scheduleRepeat(function(time) {
-        console.log('---------callback 1 fired at ' + Tone.Transport.now());
-      //console.log('transport status: ' + Tone.Transport.state);
-      //console.log('synthObj.recordedMode = ' + synthObj.recordedMode);
-      //console.log('time now: ' + Tone.Transport.now() + ', schedule callback 2 for: ' + (loopStart[key] ? ((loopStart[key] + synthObj.totalLoopOffset) - Tone.Transport.now() - 0.05) : 0.05));
-
-      // Tone.Transport.scheduleOnce(function(time) {
-      //   console.log('---------in callback 2 fired at ' + Tone.Transport.now());
-      //   loopStart[key] = Tone.Transport.now();
-      //   // check for recorded mode === false here and return right away
-      //   if (synthObj.recordedMode === false) {
-      //     return;
-      //   }
-
-        notes = synthObj.notes;
-
-        notes.forEach(function(noteObj) {
-          //if (synthObj.recordedMode) {
-            scheduleNote(synthObj.id, noteObj, false);
-          //}
-        });
-        //loopEnd[key] = synthObj.synth.now();
-    //  }, '+' + (loopStart[key] ? ((loopStart[key] + synthObj.totalLoopOffset) - Tone.Transport.now() - 0.05) : 0.05));
-      }, (synthObj.loopTime - 0.05));
+      if (synthObj.scheduled) {
+        console.log('synth: ', synthObj.id, ' already scheduled');
+      } else {
+        console.log('time now: ', Tone.Transport.now(), ', schedule periodic callback 1 for synth id: ', synthObj.id, ' every ', (synthObj.loopTime - 0.05));
+        synthObj.scheduled = Tone.Transport.scheduleRepeat(synthObj.synthScheduleCallback, (synthObj.loopTime - 0.05));
+      }
     });
 };
 
@@ -118,15 +98,23 @@ function ToneSynth(id, synth) {
   this.id = id;
   this.synth = synth;
   this.notes = [];
-  this.startTime = Tone.Transport.now(); // shouldn't need
-  this.nextTime = this.startTime + 0.05; // shouldn't need
-  this.offSet = 0; // shouldn't need
-  this.totalLoopOffset = 0; // shouldn't need
-  this.recordedMode = false;  // shouldn't need
+  this.offSet = 0;
   this.transportTimeStart = 0;  // The transport time this synth was started at
   this.curVirtTime = 0;  // The current virtual time of this synth
   this.loopTime = 0;  // The time it takes to make a loop of the notes defined in this synth
   this.offSetTicks = 0;
+
+  var thisSynth = this;
+
+  this.synthScheduleCallback = function(time) {
+
+    console.log('--------- synthScheduleCallback fired at ', Tone.Transport.now());
+
+    console.log('scheduling notes for synth: ', thisSynth.id);
+    thisSynth.notes.forEach(function(noteObj) {
+        scheduleNote(thisSynth.id, noteObj, false);
+    });
+  };
 };
 
 ToneBlockMorph.prototype = new CommandBlockMorph();
@@ -181,9 +169,10 @@ Process.prototype.toneSimpleSynth = function (body) {
 
     addSynthToToneMap(toneSynth);
     existingSynth = toneSynth;
-  } else {
-    existingSynth.recordedMode = false;
   }
+  // } else {
+  //   existingSynth.recordedMode = false;
+  // }
 
   var outer = this.context.outerContext;
   outer.expression = this.context.expression.id;
